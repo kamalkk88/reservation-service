79dca317cf896ae8d1e5561846d317e970a7ee2e
kamalkk88 <kpandey46@gmail.com>
2023-09-28 22:57:12 +0530

    third

--- src/test/java/com/hotel/reservationservice/ReservationServiceApplicationTests.java
+++ src/test/java/com/hotel/reservationservice/ReservationServiceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReservationServiceApplicationTests {
 
-    @Test
+   // @Test
     void contextLoads() {
     }
 
